b8d573079c3a5da6fbb88d3225629d87b5813db1
Shashi Kant <shashi1729kant@gmail.com>
2023-03-22 19:53:42 +0530

    UI changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-	const [win, setWin] = useState(false);
+	const [win, setWin] = useState(true);
 	const [original_number, setOriginal_Number] = useState(0);
 	const [guess_number, setGuess_Number] = useState(0);
 	const [attempts, setAttempts] = useState(0);
@@ -10,16 +10,16 @@ const App = () => {
 	const handleChange = (event) => {
 		setShowHint(false);
 		setGuess_Number(parseInt(event.target.value));
-		console.log(guess_number);
+		// console.log(guess_number);
 	};
 	const checkChange = () => {
-		console.log(guess_number, original_number);
-		console.log(attempts);
+		// console.log(guess_number, original_number);
+		// console.log(attempts);
 		setShowHint(true);
+		setAttempts(attempts + 1);
 		if (guess_number === original_number) {
 			setWin(true);
 		} else {
-			setAttempts(attempts + 1);
 		}
 	};
 	const startChanges = () => {
@@ -41,65 +41,83 @@ const App = () => {
 			<div className="flex flex-grow flex-col mt-10 gap-16">
 				<div
 					id="start"
-					className="flex w-1/4 h-12 place-items-center text-xl hover:bg-green-600 hover:cursor-pointer place-self-center justify-center rounded-lg bg-green-400"
+					className={`flex w-1/4 h-12 place-items-center text-xl hover:bg-green-600 hover:cursor-pointer place-self-center justify-center rounded-lg bg-green-400 ${
+						win ? "animate-headShake" : ""
+					} `}
 					onClick={startChanges}
 					type="button"
 				>
 					Start
 				</div>
 				<div
-					id="original_number_&_attempt"
+					id="original_number_&_guess_number"
 					className="flex flex-row h-40 w-full md:w-1/2 gap-2 place-self-center px-4"
 				>
 					<div
 						id="original_number"
-						className={`flex w-1/2 place-items-center justify-center h-full text-5xl rounded-xl transition-all duration-500 ease-in-out ${
-							win ? "bg-white text-black " : "bg-green-600"
+						className={`flex w-2/5 place-items-center justify-center h-full text-5xl rounded-xl transition-all duration-500 animate-ping-slow  ${
+							win ? " text-black " : "bg-green-600"
 						}`}
 					>
 						{win ? original_number : ""}
 					</div>
+					<div
+						id="original_number"
+						className={`flex w-1/5 place-items-center justify-center h-full text-5xl rounded-xl text-center `}
+					>
+						{showHint ? (
+							!win ? (
+								<p className="transition delay-200 animate-ping-slow">
+									{guess_number > original_number ? (
+										<div>&lt;</div>
+									) : (
+										<div>&gt;</div>
+									)}
+								</p>
+							) : (
+								<>&#61;</>
+							)
+						) : (
+							<></>
+						)}
+					</div>
+					<div
+						id="original_number"
+						className={`flex w-2/5 place-items-center justify-center h-full text-5xl`}
+					>
+						<input
+							type="number"
+							name=""
+							id="input"
+							value={guess_number}
+							className={`w-full rounded-md bg-slate-200 opacity-80 text-center text-5xl pl-2 h-full ${
+								!win ? "animate-wiggle focus:animate-none" : ""
+							}   `}
+							onChange={handleChange}
+						/>
+					</div>
+				</div>
+				<div
+					id="number_of_attempts"
+					className="flex px-4 flex-col w-full md:w-1/2 place-self-center gap-6"
+				>
 					<div
 						id="attempt"
 						className="flex w-1/2 place-items-center justify-center h-full text-3xl"
 					>
 						Attempts : {attempts}
 					</div>
-				</div>
-				<div
-					id="type_the_number"
-					className="flex px-4 flex-col w-full md:w-1/2 place-self-center gap-6"
-				>
-					<input
-						type="number"
-						name=""
-						id="input"
-						value={guess_number}
-						className="w-full rounded-md bg-slate-200 h-16 text-2xl pl-2"
-						onChange={handleChange}
-					/>
+
 					<button
 						type="button"
 						disabled={win}
-						className="w-1/4 rounded-lg bg-green-500 p-2 text-xl place-self-center hover:bg-green-700"
+						className={`w-1/4 rounded-lg bg-green-500 p-2 text-xl place-self-center  ${
+							win ? "" : "hover:bg-green-700"
+						}`}
 						onClick={checkChange}
 					>
 						Go
 					</button>
-					{showHint ? (
-						!win ? (
-							<p>
-								Hint :{" "}
-								{`Guessed Number in ${
-									guess_number > original_number ? `Large` : `Small`
-								} than Original Number`}
-							</p>
-						) : (
-							<>Well Done ! Start Again</>
-						)
-					) : (
-						<></>
-					)}
 				</div>
 			</div>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,42 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-    }
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			keyframes: {
+				headShake: {
+					"0%": {
+						transform: "translateX(0)",
+					},
+					6.5: {
+						transform: "translateX(-6px) rotateY(-9deg)",
+					},
+					"18.5%": {
+						transform: "translate(5px) rotateY(7deg)",
+					},
+					"31.5%": {
+						transform: "translate(-3px) rotateY(-5deg)",
+					},
+					"43.5%": {
+						transform: "translate(2px) rotateY(3deg)",
+					},
+					"50%": {
+						transform: "translateX(0)",
+					},
+				},
+				wiggle: {
+					"0%, 100%": {
+						transform: "rotate(-1deg)",
+					},
+					"50%": {
+						transform: "rotate(1deg)",
+					},
+				},
+			},
+			animation: {
+				headShake: "headShake 2s infinite",
+				wiggle: "wiggle 1s ease-in-out infinite",
+			},
+		},
+	},
+	plugins: [],
+};
